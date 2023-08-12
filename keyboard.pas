b468f551331e1f36750773354231c056d6264c3d
BazzaCuda <bazzacuda@gmx.com>
2023-08-12 15:20:24 +0100

    Force mpv to stop playing on exit command

--- keyboard.pas
+++ keyboard.pas
@@ -250,7 +250,7 @@ begin
   case getKeyOp of
     koNone:       EXIT; // key not processed. bypass setting result to TRUE
 
-    koCloseApp:          sendSysCommandClose(UI.handle);
+    koCloseApp:          begin MP.stop; sendSysCommandClose(UI.handle); end;
     koVolUp:             MP.volUp;
     koVolDn:             MP.volDown;
     koTab:               MP.tab(aShiftState, KB.capsLock);
