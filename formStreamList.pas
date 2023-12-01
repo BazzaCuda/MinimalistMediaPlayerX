75985a3d7c0661ea0ea8827a8fdc8e20dd0fe650
Baz Cuda <bazzacuda@gmx.com>
2023-12-01 17:40:12 +0000

    extracted TSegment class into its own unit

--- formStreamList.pas
+++ formStreamList.pas
@@ -22,7 +22,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.ControlList, generics.collections,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.ControlList, generics.collections, TSegmentClass,
   formTimeline;
 
 type
--- formTimeline.pas
+++ formTimeline.pas
@@ -22,7 +22,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.ExtCtrls, Generics.Collections, Vcl.StdCtrls, Vcl.Imaging.pngimage, formProgress;
+  Vcl.Controls, Vcl.Forms, Vcl.ExtCtrls, Generics.Collections, Vcl.StdCtrls, Vcl.Imaging.pngimage, formProgress, TSegmentClass;
 
 type
   TTimelineForm = class(TForm)
@@ -47,54 +47,6 @@ type
     procedure CreateParams(var Params: TCreateParams);
   public
     property cursorPos: integer read getCursorPos write setCursorPos;
-  end;
-
-  TSegment = class(TPanel)
-  strict private
-    FDeleted:     boolean;
-    FEndSS:       integer;
-    FOldColor:    TColor;
-    FSegDetails:  TLabel;
-    FSegID:       TLabel;
-    FSelected:    boolean;
-    FStartSS:     integer;
-    FTrashCan:    TImage;
-  private
-    function  getDuration: integer;
-    procedure setSegID(const Value: string);
-    function  getSegID: string;
-    procedure setSelected(const Value: boolean);
-    function  getIx: integer;
-    function  getIsLast: boolean;
-    function  getIsFirst: boolean;
-
-    class var FSelSeg: TSegment;
-    class var FSegments: TObjectList<TSegment>;
-    class destructor freeSegments;
-    class function getSegments: TObjectList<TSegment>; static;
-
-  protected
-    procedure doClick(Sender: TObject);
-    procedure paint; override;
-  public
-    constructor create(const aStartSS: integer; const aEndSS: integer; const aParent: TForm; const aDeleted: boolean = FALSE);
-    function delete: boolean;
-    procedure setDisplayDetails;
-    property deleted:   boolean read FDeleted  write FDeleted;
-    property duration:  integer read getDuration;
-    property endSS:     integer read FEndSS    write FEndSS;
-    property isFirst:   boolean read getIsFirst;
-    property isLast:    boolean read getIsLast;
-    property ix:        integer read getIx;
-    property oldColor:  TColor  read FOldColor write FOldColor;
-    property segID:     string  read getSegID  write setSegID;
-    property selected:  boolean read FSelected write setSelected;
-    property startSS:   integer read FStartSS  write FStartSS;
-    property trashCan:  TImage  read FTrashCan;
-
-    class function clearFocus: boolean; static;
-    class property segments: TObjectList<TSegment> read getSegments;
-    class property selSeg: TSegment read FSelSeg write FSelSeg;
   end;
 
   TTimeline = class(TObject)
@@ -151,10 +103,6 @@ function focusTimeline: boolean;
 function showTimeline(const Pt: TPoint; const aWidth: integer; const createNew: boolean = TRUE): boolean;
 function shutTimeline: boolean;
 function TL: TTimeline;
-
-const
-  NEARLY_BLACK = clBlack + $101010;
-
 
 implementation
 
@@ -235,26 +183,6 @@ function TL: TTimeline;
 begin
   case gTL = NIL of TRUE: gTL := TTimeline.create; end;
   result := gTL;
-end;
-
-var nextColor: integer = 0;
-function generateRandomEvenDarkerSoftColor: TColor;
-// chatGPT
-var
-  darkerSoftColors: array of TColor;
-begin
-  // Define an array of even darker soft colors
-  SetLength(darkerSoftColors, 6);
-  darkerSoftColors[0] := RGB(80, 80, 80);   // Very Dark Gray
-  darkerSoftColors[1] := RGB(70, 70, 70);   // Very Dark Silver
-  darkerSoftColors[2] := RGB(60, 60, 60);   // Very Dark Platinum
-  darkerSoftColors[3] := RGB(50, 50, 50);   // Very Dark Snow
-  darkerSoftColors[4] := RGB(40, 40, 40);   // Very Dark Ivory
-  darkerSoftColors[5] := RGB(30, 30, 30);   // Extremely Dark Gray
-
-  result := darkerSoftColors[nextColor];
-  inc(nextColor);
-  case nextColor > 5 of TRUE: nextColor := 0; end;
 end;
 
 {$R *.dfm}
@@ -728,139 +656,6 @@ begin
                               vSL4.text := vSL3.text;
                               FRedoList.push(vSL4); end;end;
   FUndoList.pop;
-end;
-
-{ TSegment }
-
-procedure TSegment.doClick(Sender: TObject);
-begin
-  clearFocus;
-  FSelSeg    := SELF;
-  selected   := TRUE;
-end;
-
-class function TSegment.clearFocus: boolean;
-begin
-  for var vSegment in FSegments do vSegment.selected := FALSE;
-  FSelSeg := NIL;
-end;
-
-function TSegment.delete: boolean;
-begin
-  deleted    := TRUE;
-  case color  = NEARLY_BLACK of FALSE: oldColor := color; end; // in case user tries to delete an already-deleted segment
-  color      := NEARLY_BLACK;
-end;
-
-
-constructor TSegment.create(const aStartSS: integer; const aEndSS: integer; const aParent: TForm; const aDeleted: boolean = FALSE);
-begin
-  inherited create(NIL);
-  parent            := aParent;
-  height            := 54;
-  font.color        := clSilver;
-  font.size         := 10;
-  font.style        := [fsBold];
-  alignment         := taLeftJustify;
-  onClick           := doClick;
-  doubleBuffered    := TRUE;
-
-  startSS           := aStartSS;
-  endSS             := aEndSS;
-  borderStyle       := bsNone;
-  bevelOuter        := bvNone;
-  color             := generateRandomEvenDarkerSoftColor;
-  oldColor          := color;
-
-  FSegID            := TLabel.create(SELF);
-  FSegID.parent     := SELF;
-  FSegID.top        := 0;
-  FSegID.left       := 4;
-  FSegID.styleElements := [];
-
-  FSegDetails := TLabel.create(SELF);
-  FSegDetails.parent     := SELF;
-  FSegDetails.top        := 38;
-  FSegDetails.left       := 4;
-  FSegDetails.styleElements := [];
-
-  FTrashCan := TImage.create(SELF);
-  FTrashCan.parent := SELF;
-  FTrashCan.stretch := TRUE;
-  FTrashCan.center  := TRUE;
-  FTrashCan.height  := 31;
-  FTrashCan.width   := 41;
-  FTrashCan.visible := FALSE;
-  FTrashCan.onClick := doClick;
-
-  case aDeleted of TRUE: SELF.delete; end;
-end;
-
-function TSegment.getDuration: integer;
-begin
-  result := FEndSS - FStartSS;
-end;
-
-function TSegment.getIx: integer;
-begin
-  result := FSegments.indexOf(SELF);
-end;
-
-function TSegment.getIsFirst: boolean;
-begin
-  result := ix = 0;
-end;
-
-function TSegment.getIsLast: boolean;
-begin
-  result := ix = FSegments.count - 1;
-end;
-
-function TSegment.getSegID: string;
-begin
-  result := FSegID.caption;
-end;
-
-class function TSegment.getSegments: TObjectList<TSegment>;
-begin
-  case FSegments = NIL of TRUE: begin
-                                  FSegments := TObjectList<TSegment>.create;
-                                  FSegments.ownsObjects := TRUE; end;end;
-  result := FSegments;
-end;
-
-procedure TSegment.paint;
-begin
-  var rect := getClientRect;
-
-  canvas.brush.color := color;
-
-  canvas.fillRect(rect);
-
-  case selected of  TRUE: Frame3D(canvas, rect, clTeal, clTeal, 1);
-                   FALSE: Frame3D(canvas, rect, color, color, 1); end;
-end;
-
-procedure TSegment.setDisplayDetails;
-begin
-  FSegDetails.caption := format('%ds - %ds', [startSS, endSS]);
-end;
-
-procedure TSegment.setSegID(const Value: string);
-begin
-  FSegID.caption := value;
-end;
-
-procedure TSegment.setSelected(const Value: boolean);
-begin
-  FSelected := Value;
-  invalidate;
-end;
-
-class destructor TSegment.freeSegments;
-begin
-  FSegments.clear;
-  freeAndNil(FSegments);
 end;
 
 initialization
