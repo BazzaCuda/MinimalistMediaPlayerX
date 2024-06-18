08c6f60357e5f8901d778d999fec9981a3c56201
Baz Cuda <bazzacuda@gmx.com>
2024-06-18 14:24:23 +0100

    apply coding standards to legacy code

--- TMixerClass.pas
+++ TMixerClass.pas
@@ -28,12 +28,12 @@ type
 
   TMixer = class
   protected
-    function  getMute: boolean; virtual; abstract;
-    function  getVolume: integer; virtual; abstract;
-    procedure setMute(Value: boolean); virtual; abstract;
-    procedure setVolume(Value: integer); virtual; abstract;
+    function  getMute: boolean;           virtual; abstract;
+    function  getVolume: integer;         virtual; abstract;
+    procedure setMute(aValue: boolean);   virtual; abstract;
+    procedure setVolume(aValue: integer); virtual; abstract;
   public
-    function setSysVolMax: string; virtual; abstract;
+    function setSysVolMax: string;
     property muted:  boolean read getMute   write setMute;
     property volume: integer read getVolume write setVolume;
   end;
@@ -43,261 +43,260 @@ function mmpMixer: TMixer;
 implementation
 
 uses
-  Windows, MMSystem, mmpMMDevApi_tlb, ComObj, ActiveX, SysUtils;
+  winApi.activeX, winApi.MMSystem, winApi.windows,
+  system.sysUtils, system.win.comObj,
+  mmpMMDevApi_tlb;
 
 type
 
-  TxpMixer = class(TMixer)
+  TXPMixer = class(TMixer)
   private
-    Fmxct: integer;
+    Fmxct:  integer;
     FMixer: HMIXER;
     procedure chk(r: MMRESULT);
   protected
-    function  getMute: boolean; override;
-    function  getVolume: integer; override;
-    procedure setMute(Value: boolean); override;
-    procedure setVolume(Value: integer); override;
-    function  setSysVolMax: string; override;
+    function  getMute: boolean;           override;
+    function  getVolume: integer;         override;
+    procedure setMute(aValue: boolean);   override;
+    procedure setVolume(aValue: integer); override;
   public
-    constructor Create;
+    constructor create;
     destructor Destroy; override;
   end;
 
   TVistaMixer = class(TMixer)
   private
-    FmmDev: IMMDevice;
-    FmmDevEnum: IMMDeviceEnumerator;
-    FmmEndpoint: IMMAudioEndpointVolume;
+    FmmDev:       IMMDevice;
+    FmmDevEnum:   IMMDeviceEnumerator;
+    FmmEndpoint:  IMMAudioEndpointVolume;
   protected
-    function  getMute: boolean; override;
-    function  getVolume: integer; override;
-    procedure setMute(Value: boolean); override;
-    procedure setVolume(Value: integer); override;
-    function  setSysVolMax: string; override;
+    function  getMute: boolean;           override;
+    function  getVolume: integer;         override;
+    procedure setMute(aValue: boolean);   override;
+    procedure setVolume(aValue: integer); override;
   public
-    constructor Create;
+    constructor create;
   end;
 
 var
-  _g_mixer: TMixer;
+  gMixer: TMixer;
 
 function mmpMixer: TMixer;
 var
   VerInfo: TOSVersioninfo;
 begin
-  if (_g_mixer = nil) then
-  begin
-    VerInfo.dwOSVersionInfoSize := SizeOf(TOSVersionInfo);
-    GetVersionEx(VerInfo);
-    if (VerInfo.dwMajorVersion >= 6) then
-      _g_mixer := TVistaMixer.Create
-    else
-      _g_mixer := TxpMixer.Create;
-  end;
-  result := _g_mixer;
+  case gMixer = NIL of TRUE:  begin
+                                  verInfo.dwOSVersionInfoSize := sizeOf(TOSVersionInfo);
+                                  getVersionEx(verInfo);
+                                  case verInfo.dwMajorVersion >= 6 of  TRUE: gMixer := TVistaMixer.create;
+                                                                      FALSE: gMixer := TXPMixer.create; end;end;end;
+  result := gMixer;
 end;
 
-{ TxpMixer }
+{ TXPMixer }
 
-procedure TxpMixer.chk(r: MMRESULT);
+procedure TXPMixer.chk(r: MMRESULT);
 var
   s: string;
 begin
-  if (r = MMSYSERR_NOERROR) then
-    exit;
-  setLength(s, MMSystem.MAXERRORLENGTH + 1);
-  waveOutGetErrorText(r, @s[1], MMSystem.MAXERRORLENGTH);
-  raise Exception.Create(StrPas(pChar(s)));
+  case r = MMSYSERR_NOERROR of TRUE: EXIT; end;
+  setLength(s, winApi.MMSystem.MAXERRORLENGTH + 1);
+  waveOutGetErrorText(r, @s[1], winApi.MMSystem.MAXERRORLENGTH);
+  raise exception.create(strPas(pChar(s)));
 end;
 
-constructor TxpMixer.Create;
+constructor TXPMixer.create;
 begin
   Fmxct := MIXERLINE_COMPONENTTYPE_DST_SPEAKERS;
-  chk(mixerOpen(@Fmixer, 0, 0, 0, 0));
+  chk(mixerOpen(@FMixer, 0, 0, 0, 0));
 end;
 
-destructor TxpMixer.Destroy;
+destructor TXPMixer.Destroy;
 begin
-  if (FMixer <> 0) then
-    mixerClose(FMixer);
+  case FMixer <> 0 of TRUE: mixerClose(FMixer); end;
   inherited;
 end;
 
-function TxpMixer.getMute: boolean;
+function TXPMixer.getMute: boolean;
 var
-  MasterMute: TMixerControl;
-  Details: TMixerControlDetails;
-  BoolDetails: TMixerControlDetailsBoolean;
-  Line: TMixerLine;
-  Controls: TMixerLineControls;
+  vMasterMute:  TMixerControl;
+  vDetails:     TMixerControlDetails;
+  vBoolDetails: TMixerControlDetailsBoolean;
+  vLine:        TMixerLine;
+  vControls:    TMixerLineControls;
 begin
-  ZeroMemory(@Line, SizeOf(Line));
-  Line.cbStruct := SizeOf(Line);
-  Line.dwComponentType := Fmxct;
-  chk(mixerGetLineInfo(0, @Line, MIXER_GETLINEINFOF_COMPONENTTYPE));
+  zeroMemory(@vLine, sizeOf(vLine));
+  vLine.cbStruct          := sizeOf(vLine);
+  vLine.dwComponentType   := Fmxct;
+  chk(mixerGetLineInfo(0, @vLine, MIXER_GETLINEINFOF_COMPONENTTYPE));
 
-  ZeroMemory(@Controls, SizeOf(Controls));
-  Controls.cbStruct := SizeOf(Controls);
-  Controls.dwLineID := Line.dwLineID;
-  Controls.cControls := 1;
-  Controls.dwControlType := MIXERCONTROL_CONTROLTYPE_MUTE;
-  Controls.cbmxctrl := SizeOf(MasterMute);
-  Controls.pamxctrl := @MasterMute;
-  chk(mixerGetLineControls(0, @Controls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
+  zeroMemory(@vControls, sizeOf(vControls));
+  vControls.cbStruct      := sizeOf(vControls);
+  vControls.dwLineID      := vLine.dwLineID;
+  vControls.cControls     := 1;
+  vControls.dwControlType := MIXERCONTROL_CONTROLTYPE_MUTE;
+  vControls.cbmxctrl      := sizeOf(vMasterMute);
+  vControls.pamxctrl      := @vMasterMute;
+  chk(mixerGetLineControls(0, @vControls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
 
-  Details.cbStruct := SizeOf(Details);
-  Details.dwControlID := MasterMute.dwControlID;
-  Details.cChannels := 1;
-  Details.cMultipleItems := 0;
-  Details.cbDetails := SizeOf(BoolDetails);
-  Details.paDetails := @BoolDetails;
-  chk(mixerGetControlDetails(0, @Details, MIXER_GETCONTROLDETAILSF_VALUE));
+  vDetails.cbStruct       := sizeOf(vDetails);
+  vDetails.dwControlID    := vMasterMute.dwControlID;
+  vDetails.cChannels      := 1;
+  vDetails.cMultipleItems := 0;
+  vDetails.cbDetails      := sizeOf(vBoolDetails);
+  vDetails.paDetails      := @vBoolDetails;
+  chk(mixerGetControlDetails(0, @vDetails, MIXER_GETCONTROLDETAILSF_VALUE));
 
-  result := BoolDetails.fValue <> 0;
+  result := vBoolDetails.fValue <> 0;
 end;
 
-function TxpMixer.getVolume: integer;
+function TXPMixer.getVolume: integer;
 var
-  Line: TMixerLine;
-  Controls: TMixerLineControls;
-  MasterVolume: TMixerControl;
-  Details: TMixerControlDetails;
-  UnsignedDetails: TMixerControlDetailsUnsigned;
+  vLine:            TMixerLine;
+  vControls:        TMixerLineControls;
+  vMasterVolume:    TMixerControl;
+  vDetails:         TMixerControlDetails;
+  vUnsignedDetails: TMixerControlDetailsUnsigned;
 begin
-  ZeroMemory(@Line, SizeOf(Line));
-  Line.cbStruct := SizeOf(Line);
-  Line.dwComponentType := Fmxct;
-  chk(mixerGetLineInfo(Fmixer, @Line, MIXER_GETLINEINFOF_COMPONENTTYPE));
-  ZeroMemory(@Controls, SizeOf(Controls));
-  Controls.cbStruct := SizeOf(Controls);
-  Controls.dwLineID := Line.dwLineID;
-  Controls.cControls := 1;
-  Controls.dwControlType := MIXERCONTROL_CONTROLTYPE_VOLUME;
-  Controls.cbmxctrl := SizeOf(MasterVolume);
-  Controls.pamxctrl := @MasterVolume;
-  chk(mixerGetLineControls(Fmixer, @Controls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
+  zeroMemory(@vLine, sizeOf(vLine));
+  vLine.cbStruct          := sizeOf(vLine);
+  vLine.dwComponentType   := Fmxct;
+  chk(mixerGetLineInfo(FMixer, @vLine, MIXER_GETLINEINFOF_COMPONENTTYPE));
 
-  details.cbStruct := SizeOf(Details);
-  details.dwControlID := MasterVolume.dwControlID;
-  details.cChannels := 1;
-  details.cMultipleItems := 0;
-  details.cbDetails := SizeOf(UnsignedDetails);
-  details.paDetails := @UnsignedDetails;
-  chk(mixerGetControlDetails(Fmixer, @Details, MIXER_GETCONTROLDETAILSF_VALUE));
-  result := UnsignedDetails.dwValue;
+  zeroMemory(@vControls, sizeOf(vControls));
+  vControls.cbStruct      := sizeOf(vControls);
+  vControls.dwLineID      := vLine.dwLineID;
+  vControls.cControls     := 1;
+  vControls.dwControlType := MIXERCONTROL_CONTROLTYPE_VOLUME;
+  vControls.cbmxctrl      := sizeOf(vMasterVolume);
+  vControls.pamxctrl      := @vMasterVolume;
+  chk(mixerGetLineControls(FMixer, @vControls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
+
+  vdetails.cbStruct       := sizeOf(vDetails);
+  vdetails.dwControlID    := vMasterVolume.dwControlID;
+  vdetails.cChannels      := 1;
+  vdetails.cMultipleItems := 0;
+  vdetails.cbDetails      := sizeOf(vUnsignedDetails);
+  vdetails.paDetails      := @vUnsignedDetails;
+  chk(mixerGetControlDetails(FMixer, @vDetails, MIXER_GETCONTROLDETAILSF_VALUE));
+
+  result := vUnsignedDetails.dwValue;
 end;
 
-procedure TxpMixer.setMute(Value: boolean);
+procedure TXPMixer.setMute(aValue: boolean);
 var
-  Line: TMixerLine;
-  Controls: TMixerLineControls;
-  MasterMute: TMixerControl;
-  Details: TMixerControlDetails;
-  BoolDetails: TMixerControlDetailsBoolean;
+  vLine:        TMixerLine;
+  vControls:    TMixerLineControls;
+  vMasterMute:  TMixerControl;
+  vDetails:     TMixerControlDetails;
+  vBoolDetails: TMixerControlDetailsBoolean;
 begin
-  ZeroMemory(@Line, SizeOf(Line));
-  Line.cbStruct := SizeOf(Line);
-  Line.dwComponentType := Fmxct;
-  chk(mixerGetLineInfo(Fmixer, @Line, MIXER_GETLINEINFOF_COMPONENTTYPE));
-  ZeroMemory(@Controls, SizeOf(Controls));
-  Controls.cbStruct := SizeOf(Controls);
-  Controls.dwLineID := Line.dwLineID;
-  Controls.cControls := 1;
-  Controls.dwControlType := MIXERCONTROL_CONTROLTYPE_MUTE;
-  Controls.cbmxctrl := SizeOf(masterMute);
-  Controls.pamxctrl := @masterMute;
-  chk(mixerGetLineControls(Fmixer, @Controls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
+  zeroMemory(@vLine, sizeOf(vLine));
+  vLine.cbStruct          := sizeOf(vLine);
+  vLine.dwComponentType   := Fmxct;
+  chk(mixerGetLineInfo(FMixer, @vLine, MIXER_GETLINEINFOF_COMPONENTTYPE));
 
-  details.cbStruct := SizeOf(Details);
-  details.dwControlID := MasterMute.dwControlID;
-  details.cChannels := 1;
-  details.cMultipleItems := 0;
-  details.cbDetails := SizeOf(BoolDetails);
-  details.paDetails := @BoolDetails;
-  mixerGetControlDetails(0, @Details, MIXER_GETCONTROLDETAILSF_VALUE);
-  if (Value) then
-    BoolDetails.fValue := 1
-  else
-    BoolDetails.fValue := 0;
+  zeroMemory(@vControls, sizeOf(vControls));
+  vControls.cbStruct      := sizeOf(vControls);
+  vControls.dwLineID      := vLine.dwLineID;
+  vControls.cControls     := 1;
+  vControls.dwControlType := MIXERCONTROL_CONTROLTYPE_MUTE;
+  vControls.cbmxctrl      := sizeOf(vMasterMute);
+  vControls.pamxctrl      := @vMasterMute;
+  chk(mixerGetLineControls(FMixer, @vControls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
 
-  chk(mixerSetControlDetails(0, @Details, MIXER_SETCONTROLDETAILSF_VALUE));
+  vDetails.cbStruct       := sizeOf(vDetails);
+  vDetails.dwControlID    := vMasterMute.dwControlID;
+  vDetails.cChannels      := 1;
+  vDetails.cMultipleItems := 0;
+  vDetails.cbDetails      := sizeOf(vBoolDetails);
+  vDetails.paDetails      := @vBoolDetails;
+  mixerGetControlDetails(0, @vDetails, MIXER_GETCONTROLDETAILSF_VALUE);
+
+  case aValue of   TRUE: vBoolDetails.fValue := 1;
+                  FALSE: vBoolDetails.fValue := 0; end;
+
+  chk(mixerSetControlDetails(0, @vDetails, MIXER_SETCONTROLDETAILSF_VALUE));
 end;
 
-function TxpMixer.setSysVolMax: string;
-begin
-  result := '';
-  setMute(FALSE);
-  setVolume(65535);
-  result := 'Sys Vol Max';
-end;
-
-procedure TxpMixer.setVolume(Value: integer);
+procedure TXPMixer.setVolume(aValue: integer);
 var
-  Line: TMixerLine;
-  Controls: TMixerLineControls;
-  MasterVolume: TMixerControl;
-  Details: TMixerControlDetails;
-  UnsignedDetails: TMixerControlDetailsUnsigned;
+  vLine:            TMixerLine;
+  vControls:        TMixerLineControls;
+  vMasterVolume:    TMixerControl;
+  vDetails:         TMixerControlDetails;
+  vUnsignedDetails: TMixerControlDetailsUnsigned;
 begin
-  if (value < 0) then
-    value := 0;
-  if (value > 65535) then
-    value := 65535;
+  case aValue < 0     of TRUE: aValue := 0;     end;
+  case aValue > 65535 of TRUE: aValue := 65535; end;
 
-  ZeroMemory(@Line, SizeOf(Line));
-  Line.cbStruct := SizeOf(Line);
-  Line.dwComponentType := Fmxct;
-  chk(mixerGetLineInfo(Fmixer, @Line, MIXER_GETLINEINFOF_COMPONENTTYPE));
-  ZeroMemory(@Controls, SizeOf(Controls));
-  Controls.cbStruct := SizeOf(Controls);
-  Controls.dwLineID := Line.dwLineID;
-  Controls.cControls := 1;
-  Controls.dwControlType := MIXERCONTROL_CONTROLTYPE_VOLUME;
-  Controls.cbmxctrl := SizeOf(MasterVolume);
-  Controls.pamxctrl := @MasterVolume;
-  chk(mixerGetLineControls(Fmixer, @Controls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
+  zeroMemory(@vLine, sizeOf(vLine));
+  vLine.cbStruct          := sizeOf(vLine);
+  vLine.dwComponentType   := Fmxct;
+  chk(mixerGetLineInfo(FMixer, @vLine, MIXER_GETLINEINFOF_COMPONENTTYPE));
 
-  details.cbStruct := SizeOf(Details);
-  details.dwControlID := MasterVolume.dwControlID;
-  details.cChannels := 1;
-  details.cMultipleItems := 0;
-  details.cbDetails := SizeOf(UnsignedDetails);
-  details.paDetails := @UnsignedDetails;
-  UnsignedDetails.dwValue := Value;
-  chk(mixerSetControlDetails(Fmixer, @Details, MIXER_SETCONTROLDETAILSF_VALUE));
+  zeroMemory(@vControls, sizeOf(vControls));
+  vControls.cbStruct      := sizeOf(vControls);
+  vControls.dwLineID      := vLine.dwLineID;
+  vControls.cControls     := 1;
+  vControls.dwControlType := MIXERCONTROL_CONTROLTYPE_VOLUME;
+  vControls.cbmxctrl      := sizeOf(vMasterVolume);
+  vControls.pamxctrl      := @vMasterVolume;
+  chk(mixerGetLineControls(FMixer, @vControls, MIXER_GETLINECONTROLSF_ONEBYTYPE));
+
+  vDetails.cbStruct       := sizeOf(vDetails);
+  vDetails.dwControlID    := vMasterVolume.dwControlID;
+  vDetails.cChannels      := 1;
+  vDetails.cMultipleItems := 0;
+  vDetails.cbDetails      := sizeOf(vUnsignedDetails);
+  vDetails.paDetails      := @vUnsignedDetails;
+  vUnsignedDetails.dwValue := aValue;
+  chk(mixerSetControlDetails(FMixer, @vDetails, MIXER_SETCONTROLDETAILSF_VALUE));
 end;
 
 { TVistaMixer }
 
 constructor TVistaMixer.Create;
 begin
-  CoCreateInstance(CLSID_MMDeviceEnumerator, nil, CLSCTX_ALL, IID_IMMDeviceEnumerator, FmmDevEnum);
+  CoCreateInstance(CLSID_MMDeviceEnumerator, NIL, CLSCTX_ALL, IID_IMMDeviceEnumerator, FmmDevEnum);
   FmmDevEnum.GetDefaultAudioEndpoint(eRender, eMultimedia, FmmDev);
-  FmmDev.Activate(IID_IAudioEndpointVolume, CLSCTX_ALL, nil, FmmEndpoint);
+  FmmDev.Activate(IID_IAudioEndpointVolume, CLSCTX_ALL, NIL, FmmEndpoint);
 end;
 
 function TVistaMixer.getMute: boolean;
 var
   vResult: integer;
 begin
-  FmmEndpoint.GetMute(vResult);
+  FmmEndpoint.getMute(vResult);
   result := boolean(vResult);
 end;
 
 function TVistaMixer.getVolume: integer;
 var
-  VolLevel: Single;
+  vVolLevel: single;
 begin
-  FmmEndpoint.GetMasterVolumeLevelScalar(VolLevel);
-  result := Round(VolLevel * 65535);
+  FmmEndpoint.getMasterVolumeLevelScalar(vVolLevel);
+  result := round(vVolLevel * 65535);
 end;
 
-procedure TVistaMixer.setMute(Value: boolean);
+procedure TVistaMixer.setMute(aValue: boolean);
 begin
-  FmmEndpoint.SetMute(integer(Value), nil);
+  FmmEndpoint.setMute(integer(aValue), NIL);
 end;
 
-function TVistaMixer.setSysVolMax: string;
+procedure TVistaMixer.setVolume(aValue: integer);
+var
+  vValue: single;
+begin
+  case aValue < 0     of TRUE: aValue := 0;     end;
+  case aValue > 65535 of TRUE: aValue := 65535; end;
+  vValue := aValue / 65535;
+  FmmEndpoint.setMasterVolumeLevelScalar(vValue, NIL);
+end;
+
+{ TMixer }
+
+function TMixer.setSysVolMax: string;
 begin
   result := '';
   setMute(FALSE);
@@ -305,22 +304,10 @@ begin
   result := 'Sys Vol Max';
 end;
 
-procedure TVistaMixer.setVolume(Value: integer);
-var
-  fValue: Single;
-begin
-  if (value < 0) then
-    value := 0;
-  if (value > 65535) then
-    value := 65535;
-  fValue := Value / 65535;
-  FmmEndpoint.SetMasterVolumeLevelScalar(fValue, nil);
-end;
-
 initialization
-  _g_mixer := NIL;
+  gMixer := NIL;
 
 finalization
-  case assigned(_g_mixer) of TRUE: _g_mixer.Free; end;
+  case gMixer = NIL of FALSE: gMixer.Free; end;
 
 end.
