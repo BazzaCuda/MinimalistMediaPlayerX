1fe842b6f71331a62ecb5bc9436b27b309d68e2d
Baz Cuda <BazzaCuda@users.noreply.github.com>
2026-01-10 21:14:03 +0000

    use relative path for .\_dprBackups

--- _buildEventsRelease.bat
+++ _buildEventsRelease.bat
@@ -19,8 +19,8 @@ echo. >> %logFile%
 
 rem backup DPR file if backup folder exists
 @echo copy %3 "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" >> %logFile% 2>&1
-if exist "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" (
-    copy /Y %~3 "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" >> %logFile% 2>&1
+if exist ".\_dprBackups\" (
+    copy /Y %~3 ".\_dprBackups\" >> %logFile% 2>&1
 )
 echo *** >> %logFile%
 echo. >> %logFile%
