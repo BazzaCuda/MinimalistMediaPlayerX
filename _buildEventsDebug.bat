8d972a12c33b1a8406cd4e4a8b31e147f9825de4
Baz Cuda <BazzaCuda@users.noreply.github.com>
2026-01-10 21:07:53 +0000

    move debug and release build events to batch files

---
+++ _buildEventsDebug.bat
@@ -0,0 +1,48 @@
+@echo on
+
+set logFile="B:\Win64_Dev\Programs\MediaPlayerX\_buildEvents.log"
+echo *** Debug Build Event Called: %date% %time% *** >> %logFile%
+echo. >> %logFile%
+
+echo EXE: %1 >> %logFile%
+echo MAP: %2 >> %logFile%
+echo DPR: %3 >> %logFile%
+echo. >> %logFile%
+
+rem copy EXE to local Release folder
+@echo copy %1 "B:\Win64_Dev\Programs\MediaPlayerX\Win64\Release\" >> %logFile% 2>&1
+if exist "B:\Win64_Dev\Programs\MediaPlayerX\Win64\Release\" (
+    copy /Y "%~1" "B:\Win64_Dev\Programs\MediaPlayerX\Win64\Release\" >> %logFile% 2>&1
+)
+@echo --- >> %logFile%
+
+rem copy MAP to local Release folder
+@echo copy %2 "B:\Win64_Dev\Programs\MediaPlayerX\Win64\Release\" >> %logFile% 2>&1
+if exist "B:\Win64_Dev\Programs\MediaPlayerX\Win64\Release\" (
+    copy /Y %~2 "B:\Win64_Dev\Programs\MediaPlayerX\Win64\Release\" >> %logFile% 2>&1
+)
+@echo --- >> %logFile%
+
+rem copy EXE to X:\MMP if X: exists
+@echo copy %1 "X:\MMP\" >> %logFile% 2>&1
+if exist "X:\" (
+    copy /Y %~1 "X:\MMP\" >> %logFile% 2>&1
+)
+@echo --- >> %logFile%
+
+rem copy MAP to X:\MMP if X: exists
+@echo copy %2 "X:\MMP\" >> %logFile% 2>&1
+if exist "X:\" (
+    copy /Y %~2 "X:\MMP\" >> %logFile% 2>&1
+)
+@echo --- >> %logFile%
+
+rem backup DPR file if backup folder exists
+@echo copy %3 "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" >> %logFile% 2>&1
+if exist "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" (
+    copy /Y %~3 "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" >> %logFile% 2>&1
+)
+echo *** >> %logFile%
+echo. >> %logFile%
+
+
---
+++ _buildEventsRelease.bat
@@ -0,0 +1,26 @@
+@echo on
+
+set logFile="B:\Win64_Dev\Programs\MediaPlayerX\_buildEvents.log"
+echo *** Release Build Event Called: %date% %time% *** >> %logFile%
+echo. >> %logFile%
+
+echo EXE: %1 >> %logFile%
+echo MAP: %2 >> %logFile%
+echo DPR: %3 >> %logFile%
+echo. >> %logFile%
+
+rem Copy EXE to MinimalistMediaPlayer_v5_4_ if folder exists
+echo copy %1 "B:\Win64_Dev\Programs\MediaPlayerX\MinimalistMediaPlayer_v5_4_\" >> %logFile% 2>&1
+if exist "B:\Win64_Dev\Programs\MediaPlayerX\MinimalistMediaPlayer_v5_4_\" (
+    copy /Y %~1 "B:\Win64_Dev\Programs\MediaPlayerX\MinimalistMediaPlayer_v5_4_\" >> %logFile% 2>&1
+)
+@echo --- >> %logFile%
+echo. >> %logFile%
+
+rem backup DPR file if backup folder exists
+@echo copy %3 "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" >> %logFile% 2>&1
+if exist "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" (
+    copy /Y %~3 "B:\Win64_Dev\Programs\MediaPlayerX\_dprBackups\" >> %logFile% 2>&1
+)
+echo *** >> %logFile%
+echo. >> %logFile%
